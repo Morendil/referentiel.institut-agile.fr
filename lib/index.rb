2740283af7659119abbdfa8605853daf35fef984
Laurent Bossavit <laurent@BigMac.local>
2011-03-24 18:50:11 +0100

    Remove dashboard

--- lib/index.rb
+++ lib/index.rb
@@ -24,10 +24,3 @@ m = Mustache.new
 m[:parts] = r.all_by_type
 m[:order] = "type"
 File.open("src/index_type.ham","w") {|f| f.write m.render}
-
-Mustache.template_file = "views/dash.tmpl"
-m = Mustache.new
-m[:parts] = r.all_by_type.map {|part| {:group => part[:group]+"s", :full => part[:values].select{|v|v["full"]}.length, :total => part[:values].length}}
-m[:parts] << {:group => "Total", :full => r.all.select{|v|v["full"]}.length, :total => r.all.length}
-m[:parts].each {|each| each[:pct] = sprintf( "%0.0f",each[:full]*100.0/each[:total])}
-File.open("src/dash.ham","w") {|f| f.write m.render}
